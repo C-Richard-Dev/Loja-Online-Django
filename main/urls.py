502625067aadf475d8f931bdb64bd5036ebc8f99
Caio Richard <richarddeabreu2017@gmail.com>
2025-03-04 10:44:03 -0300

    iniciando a pagina category

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,15 @@
 # URLS MAIN
 
-from django.urls import path
+from django.urls import path,include
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home,name='home'),
+    path('category-list/', views.category_list,name='category_list'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
